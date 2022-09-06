869518227887af252b63851547b1d1cd515442f6
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2022-09-06 14:59:05 +0500

    Correct Wiren Board spelling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(name="wb-connection-manager",
       version="1.0",
-      description="network connections management service for WirenBoard devices",
+      description="network connections management service for Wiren Board devices",
       author="Petr Krasnoshchekov",
       author_email="petr.krasnoshchekov@wirenboard.ru",
       packages=['wb.connection_manager']
